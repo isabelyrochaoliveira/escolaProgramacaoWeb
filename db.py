0edd61b07e341f243d676b3feafc7a35ac39197b
Isabely Rocha <isabelyrochadeoliveira2@gmail.com>
2026-08-21 19:54:46 +0000

    Atualização do bd

--- db.py
+++ db.py
@@ -1,4 +1,3 @@
-import json
 import os
 from google.cloud import firestore
 from google.oauth2 import service_account
@@ -6,22 +5,17 @@ import streamlit as st
 
 def get_firestore_client():
     try:
-        if "FIREBASE_JSON" in st.secrets:
-            info = json.loads(st.secrets["FIREBASE_JSON"])
+        if "firebase" in st.secrets:
+            info = dict(st.secrets["firebase"]) 
             creds = service_account.Credentials.from_service_account_info(info)
             return firestore.Client(credentials=creds)
-    except FileNotFoundError:
-        pass 
-    except json.JSONDecodeError:
-        st.error("Erro na nuvem: O texto do FIREBASE_JSON está faltando alguma aspa ou vírgula.")
-        st.stop()
     except Exception:
-        pass
+        pass 
 
     if os.path.exists("firebase.json"):
         return firestore.Client.from_service_account_json("firebase.json")
 
-    st.error("Credenciais não encontradas. Verifique o secrets no Cloud ou o firebase.json local.")
+    st.error("Credenciais não encontradas. Verifique os secrets no Cloud ou o firebase.json local.")
     st.stop()
 
 db = get_firestore_client()
